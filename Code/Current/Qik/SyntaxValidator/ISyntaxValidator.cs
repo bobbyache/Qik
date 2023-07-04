53c2d9251fd226dcb44b60d5a5684bb152d876ee
bobbyache <robbie.a.blake@gmail.com>
2023-07-04 17:19:22 +0200

    Commented out error handling stuff. Isn't being used anyhow.

--- Code/Current/Qik/SyntaxValidator/ISyntaxValidator.cs
+++ Code/Current/Qik/SyntaxValidator/ISyntaxValidator.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿// using System;
 
-namespace CygSoft.Qik
-{
-    public interface ISyntaxValidator
-    {
-        event EventHandler<SyntaxErrorEventArgs> CompileError;
-        bool HasErrors { get; }
-        void Validate(string scriptText);
-    }
-}
+// namespace CygSoft.Qik
+// {
+//     public interface ISyntaxValidator
+//     {
+//         event EventHandler<SyntaxErrorEventArgs> CompileError;
+//         bool HasErrors { get; }
+//         void Validate(string scriptText);
+//     }
+// }
--- Code/Current/Qik/SyntaxValidator/SyntaxErrorEventArgs.cs
+++ Code/Current/Qik/SyntaxValidator/SyntaxErrorEventArgs.cs
@@ -1,31 +1,31 @@
-﻿using System;
+﻿// using System;
 
-namespace CygSoft.Qik
-{
-    public class SyntaxErrorEventArgs
-    {
-        public int Line { get; }
-        public int Column { get; }
-        public string Message { get; }
-        public string OffendingSymbol { get; }
-        public string Location { get; }
+// namespace CygSoft.Qik
+// {
+//     public class SyntaxErrorEventArgs
+//     {
+//         public int Line { get; }
+//         public int Column { get; }
+//         public string Message { get; }
+//         public string OffendingSymbol { get; }
+//         public string Location { get; }
 
-        public SyntaxErrorEventArgs(string location, int line, int column, string offendingSymbol, string message)
-        {
-            Line = line;
-            Column = column;
-            Message = message;
-            OffendingSymbol = offendingSymbol;
-            Location = location;
-        }
+//         public SyntaxErrorEventArgs(string location, int line, int column, string offendingSymbol, string message)
+//         {
+//             Line = line;
+//             Column = column;
+//             Message = message;
+//             OffendingSymbol = offendingSymbol;
+//             Location = location;
+//         }
 
-        public SyntaxErrorEventArgs(Exception exception)
-        {
-            Line = 0;
-            Column = 0;
-            Message = exception.Message;
-            OffendingSymbol = "";
-            Location = "Main Template";
-        }
-    }
-}
+//         public SyntaxErrorEventArgs(Exception exception)
+//         {
+//             Line = 0;
+//             Column = 0;
+//             Message = exception.Message;
+//             OffendingSymbol = "";
+//             Location = "Main Template";
+//         }
+//     }
+// }
